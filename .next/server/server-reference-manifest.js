9fb7a342d1bcbc0408a4ef4dc17980d7a06d444f
phamdat <phamdat@phamdats-MacBook-Pro.local>
2023-11-17 11:50:12 +0700

    update build code

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"WhdUMTz457ocwjwZDpsExpUBa6FHfNge/B2hryIQk4U=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"9u6KZqd9hLWUioEIcmRvAtygjENMxT+jPEL+LuUFbis=\"}"
--- components/Header.tsx
+++ components/Header.tsx
@@ -25,7 +25,7 @@ const callsToAction = [
   { name: 'Contact sales', href: '#', icon: PhoneIcon },
 ]
 
-function classNames(...classes) {
+function classNames(classes: any) {
   return classes.filter(Boolean).join(' ')
 }
 
@@ -149,7 +149,7 @@ export default function Header() {
                       <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
                         Product
                         <ChevronDownIcon
-                          className={classNames(open ? 'rotate-180' : '', 'h-5 w-5 flex-none')}
+                          className={classNames(open ? 'rotate-180' : 'h-5 w-5 flex-none')}
                           aria-hidden="true"
                         />
                       </Disclosure.Button>
